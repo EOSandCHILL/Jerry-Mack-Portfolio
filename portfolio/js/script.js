5ed4ea88b474ccb668057c367d3ecbdb6b2881a3
MackInTech <94152169+EOSandCHILL@users.noreply.github.com>
2022-06-16 08:24:42 -0400

    Add files via upload

---
+++ portfolio/js/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+  $("#slides").superslides({
+    animation: "fade",
+    play: 5000,
+    pagination: false,
+  });
+
+  var typed = new Typed(".typed", {
+    strings: [
+      "Software Engineer",
+      "Software Engineering, B.S.",
+      "Marine Corps Avionics Tech",
+      "Aeronautics, A.S.",
+    ],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
+  });
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      938: {
+        items: 4,
+      },
+    },
+  });
+
+  var skillsTopOffset = $(".skillsSection").offset().top;
+
+  $(window).scroll(function () {
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      $(".chart").easyPieChart({
+        easing: "easeInOut",
+        barColor: "rgb(0, 0, 0)",
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function (from, to, percent) {
+          $(this.el).find(".percent").text(Math.round(percent));
+        },
+      });
+    }
+  });
+});
